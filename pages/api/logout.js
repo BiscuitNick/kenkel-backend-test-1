0440fff2f65756d4a8ff8a018073d38e332051c9
BiscuitNick <nick@biscuit.land>
2022-01-21 17:45:35 -0800

    api cleanup

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -4,7 +4,7 @@ import { sessionOptions } from "../../lib/session";
 export default withIronSessionApiRoute(async function logoutRoute(req, res) {
   req.session.user = null;
   req.session.destroy();
-  await req.session.save();
+  // await req.session.save();
 
   res
     .status(200)
